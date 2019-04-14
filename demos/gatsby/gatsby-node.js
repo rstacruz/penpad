241caaca36d0ef040fadd56eda521597cf447d1d
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-04-15 00:27:23 +0800

    Use hot-loader in Gatsby

---
+++ demos/gatsby/gatsby-node.js
@@ -0,0 +1,10 @@
+// https://github.com/gatsbyjs/gatsby/issues/11934#issuecomment-469046186
+exports.onCreateWebpackConfig = ({ getConfig, stage }) => {
+  const config = getConfig()
+  if (stage.startsWith('develop') && config.resolve) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'react-dom': '@hot-loader/react-dom'
+    }
+  }
+}
