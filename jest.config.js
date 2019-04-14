d7007756f10761bdea980b4ed534474fc31d3793
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-04-14 21:58:46 +0800

    Fix test tools

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
-    '^.+\\.svg?$': 'jest-svg-transformer'
+    '^.+\\.jsx?$': 'ts-jest'
   },
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
@@ -10,7 +10,6 @@ module.exports = {
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/file-mock.js'
   },
-  setupFiles: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['node_modules', '.cache'],
   transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
   globals: {
--- packages/penpad/src/icons/index.ts
+++ packages/penpad/src/icons/index.ts
@@ -1,17 +1,13 @@
-// @ts-ignore
 export {
   default as FileOutlineIcon
 } from 'clarity-icons-svg/essential/file-outline.svg'
 
-// @ts-ignore
 export {
   default as DocIcon
 } from 'clarity-icons-svg/technology/container-outline.svg'
 
-// @ts-ignore
 export {
   default as LibraryIcon
 } from 'clarity-icons-svg/essential/library-outline.svg'
 
-// @ts-ignore
 export { default as Chevron } from 'clarity-icons-svg/core/angle-outline.svg'
