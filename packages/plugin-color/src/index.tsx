96dba77a06d052e7e32c4bda6d8e74526c7fae16
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-04-17 01:19:36 +0800

    Add stubs for plugin-color

--- packages/plugin-color/src/index.tsx
+++ packages/plugin-color/src/index.tsx
@@ -1,2 +1,10 @@
-const hello = ':)'
-export default hello
+const ColorPlugin = () => {
+  return null
+}
+
+const ColorSpecimen = () => {
+  return null
+}
+
+export { ColorPlugin, ColorSpecimen }
+export default ColorPlugin
